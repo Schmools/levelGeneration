c6655ca4202acce2d0e5e9279ea3ce201de8a905
Samuel Schuur <schuursamuel@gmail.com>
2021-04-19 11:54:29 -0400

    updated tiles class

--- wangTiles/tiles.py
+++ wangTiles/tiles.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python
+#!usr/bin/env python
 from scipy import *
+from typing import *
 
-class map:
-    def __init__(self, size):
-       allTiles = [tile() for n in range(size)]
-       adjacencyMatrix = rectangularGride(size)
+class Map:
+    def __init__(self, size: int):
+       self.allTiles = [tile() for n in range(size)]
+       self.adjacencyMatrix = rectangularGrid(size)
 
-    def rectangularGrid(self, size):
+    def rectangular_grid(self, size: int):
         length = size*size
         matrix = numpy.zeros((length, length))
 
@@ -19,8 +20,16 @@ class map:
             matrix[n+size][n] = 1
             matrix[n-size][n] = 1
 
-class tile:
-    def __init__ (self, edges):
-        #edgeColoring ought to be a list of duples (length, color)
-        #figure out type hinting
-        edgeColoring = []
+class Tile:
+    def __init__(self, edges: int):
+        #edgeColoring ought to be a tuple of duples (length, color)
+        self.edgeColoring = []
+
+    def get_edge(self, edge: int):
+        return self.edgeColoring[edge]
+    
+    def set_edge_coloring(self, edge: int, edgeColor: Edge):
+        self.edgeColoring[edge] = edgeColor
+
+    class Edge:
+    #define how to color edges
